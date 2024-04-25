fa420e90d921461d22978d321a8dad4d2389e793
Danny Sung <danny@dannysung.com>
2024-04-24 20:33:18 -0700

    Create new RotaryKnobExample app from Xcode 15.3 template

---
+++ RotaryKnobExample/RotaryKnobExample/RotaryKnobExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotaryKnobExampleApp.swift
+//  RotaryKnobExample
+//
+//  Created by Danny Sung on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct RotaryKnobExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
